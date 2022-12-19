48f04c57696386803e737239b1deb664b7c23653
Aliyeyyy <aliyeyyy35@gmail.com>
2022-12-19 22:34:00 +0100

    push

---
+++ src/test/java/Day5_LocatorPraktice/C05_Checkboxes.java
@@ -0,0 +1,17 @@
+package Day5_LocatorPraktice;
+
+public class C05_Checkboxes {
+
+
+    //    Go to URL: https://demoqa.com/
+    //    Click on Elements.
+    //    Click on Checkbox.
+    //    Verify if Home checkbox is selected.
+    //    Verify that "You have selected" is visible.
+
+
+
+
+
+
+}
